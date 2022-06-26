68a57e5cc2910b86b14757850ca3cd5c18d41d05
lee-jjunny <lee.jjunny@gmail.com>
2022-06-26 19:05:26 +0900

    20220626 - 다리를지나는트럭

--- level2/다리를지나는트럭.js
+++ level2/다리를지나는트럭.js
@@ -17,21 +17,33 @@
  * 모든 트럭의 무게는 1 이상 weight 이하입니다.
  */
 
- function solution(bridge_length, weight, truck_weights) {
-    var answer = 0;
-    let nowWeight = 0;
-    let result = [];
+function solution(bridge_length, weight, truck_weights) {
+    // '다리'를 모방한 큐에 간단한 배열로 정리 : [트럭무게, 트럭이 나갈 시간].
+    let time = 0;
+    let qu = [[0, 0]];
+    let weightOnBridge = 0;
 
-    truck_weights.forEach((value) => {
-        nowWeight+=value;
-        if(result)
-        result.push(value);
+    // 대기 트럭, 다리를 건너는 트럭이 모두 0일 때 까지 루프 반복
+    while (qu.length > 0 || truck_weights.length > 0) {
+        // 현재 시간과 큐 맨 앞의 차의 '나갈 시간'이 같다면, 큐에서 트럭을 제거하고, 다리 위 트럭 무게합에서도 뺌
+        if (qu[0][1] === time) weightOnBridge -= qu.shift()[0];
 
-        
-    })
-    
-
-    return answer;
+        if (weightOnBridge + truck_weights[0] <= weight) {
+            // 다리 위 무게 총합 + 대기중인 트럭의 첫 무게가 다리에 올라올수 있는 무게 이하면,
+            // 다리 위 무게 업데이트
+            weightOnBridge += truck_weights[0];
+            // 트럭 큐에 [트럭무게, 트럭이 나갈 시간] 추가
+            qu.push([truck_weights.shift(), time + bridge_length]);
+        } else {
+            // 3. 다음 트럭이 못올라오는 상황이면 얼른 큐의
+            // 대기중인 트럭이 올라올수 없다면 큐의 첫번째 트럭이 빠질 수 있도록 시간 증가 시킴
+            // if 밖에서 1 더하기 때문에 -1 해줌
+            if (qu[0]) time = qu[0][1] - 1;
+        }
+        // 시간 업데이트
+        time++;
+    }
+    return time;
 }
 
 /* 테스트 */
